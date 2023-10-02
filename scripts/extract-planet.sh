f16582fecd1f87280870f4d0229bc7af85c1f471
sliu2017 <liu.sonnys@gmail.com>
2023-10-02 03:08:26 -0400

    Modified extract-planet script

--- scripts/extract-planet.sh
+++ scripts/extract-planet.sh
@@ -13,6 +13,7 @@ lat=-90
 long=-180
 lat_seg=1
 long_seg=1
+segment_size=12
 
 if [[ $# -eq 0 ]]
 then
@@ -36,12 +37,16 @@ epoch_time=`date +"%s"`
 
 echo "Extracting ${planet_file}..."
 
-while [[ $lat_seg -le 180 ]] ; do
-  while [[ $long_seg -le 360 ]] ; do
-    lat_pos=$((lat+lat_seg-1))
-    long_pos=$((long+long_seg-1))
-    lat_next_pos=$((lat+lat_seg))
-    long_next_pos=$((long+long_seg))
+lat_seg_count=$((180/segment_size))
+long_seg_count=$((360/segment_size))
+while [[ $lat_seg -le lat_seg_count ]] ; do
+  while [[ $long_seg -le long_seg_count ]] ; do
+    lat_pre_seg=$((lat_seg-1))
+    long_pre_seg=$((long_seg-1))
+    lat_pos=$((lat+lat_pre_seg*segment_size))
+    long_pos=$((long+long_pre_seg*segment_size))
+    lat_next_pos=$((lat+lat_seg*segment_size))
+    long_next_pos=$((long+long_seg*segment_size))
     echo "Generating segment ${lat_seg}-${long_seg} at bbox ${lat_pos},${long_pos},${lat_next_pos},${long_next_pos}..."
     osmium extract -b $long_pos,$lat_pos,$long_next_pos,$lat_next_pos $planet_file -o planet-extract-$epoch_time-$lat_seg-$long_seg.osm.pbf -O
     long_seg=$((long_seg+1))
